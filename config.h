61289c079188d953a6cc3308d18d4b0ee49b6902
Benjamin Paul Suntrup <benjamin.p.suntrup@gmail.com>
2023-11-24 11:21:39 -0600

    unmap alt-l, it conflicts with my tmux config

--- config.h
+++ config.h
@@ -280,7 +280,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_J,           zoom,           {.f = -1} },
 	{ TERMMOD,              XK_U,           zoom,           {.f = +2} },
 	{ TERMMOD,              XK_D,           zoom,           {.f = -2} },
-	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },
+    /* TODO: map this to another key. This conflicts with my tmux config for now */
+/*	{ MODKEY,               XK_l,           externalpipe,   {.v = openurlcmd } },*/
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 };
